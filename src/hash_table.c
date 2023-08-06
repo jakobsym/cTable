f0de78f3e5c08753ab32a043080bda1ce128b83a
jakobsym <jakob.symchych@my.utsa.edu>
2023-08-06 12:01:26 -0500

    created creation of new hashtable function, as well as function for adding items(key/value) to a hash table

--- src/hash_table.c
+++ src/hash_table.c
@@ -2,11 +2,23 @@
 #include <string.h>
 #include "hash_table.h"
 
-
-// initialisation function for 'ht_item(s)'
+// Initialisation function for 'ht_item(s)'
+/**
+ * */
 static ht_item* ht_new_item(const char* k, const char *v) {
     ht_item* i = malloc(sizeof(ht_item));
     i->key = strdup(k); // `strdup()` creates char* pointer that is null-terminated
     i->value = strdup(v);
     return i;
+}
+
+// Create a new hash table: ('NULL' indicates empty bucket)
+/**
+ * */
+ht_hash_table* ht_new() {
+    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+    ht->count = 0;
+    ht->size = 53;
+    ht->items = ; // fill array of items memory with 'NULL' bytes
+    return ht;
 }
