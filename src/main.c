1f06bb12facf35ee7e00fadf1a2b63d2c74511f9
jakobsym <jakob.symchych@my.utsa.edu>
2023-08-09 16:50:49 -0500

    testing functions

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "hash_table.h"
+
+ht_hash_table* ht_new();
+void ht_del_hash_table(ht_hash_table* ht);
+
+int main() {
+    ht_hash_table* ht = ht_new();
+    ht_del_hash_table(ht);
+
+    return 0;
+}
+
